9f99ef9a67fc514f89baa4897917eabf92cdffab
Pratik Senapati <90540195+CriMsonTYpo@users.noreply.github.com>
2023-01-01 23:17:45 +0530

    add prev and next functionality
    Doesnt work yet, have to iron errors

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ let i=document.querySelector("#img");
 let k;
 let a=document.querySelectorAll(".i");
 let prev=document.querySelector("#prev");
-
+let next=document.querySelector("#next");
+let img=[i1.src, i2.src, i3.src, i4.src,i5.src];
 function change(item)
 {
     i.src=item.src;
 }
+
 
 i1.addEventListener('click', function()
 {
@@ -37,3 +39,35 @@ i5.addEventListener('click', function()
 {
     change(i5);
 })
+
+function next_()
+{
+   let s=img.findIndex(i.src);
+
+   if(s==4)
+   {
+        i.src=img[0];
+        return;
+   }
+        i.src=img[++s];
+        return;
+
+
+}
+
+function prev_()
+{
+    let s=img.findIndex(i.src);
+
+   if(s==0)
+   {
+        i.src=img[4];
+        return;
+   }
+        i.src=img[--s];
+        return;
+}
+
+next.addEventListener('click', next_())
+
+prev.addEventListener('click', prev_())
